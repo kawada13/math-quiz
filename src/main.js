53fad890053f78a95f2700ba126c01dda1f7b387
kawada <vgnsl94@gmail.com>
2021-01-28 22:23:22 +0900

    s

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+import 'font-awesome/css/font-awesome.min.css' 
+import 'bootstrap/dist/css/bootstrap.css' 
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <hr>
+    <input v-model="msg">
+    <p>・変数valueの中身： {{msg}}</p>
   </div>
 </template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      msg: 'Default message'
+    }
+  },
+}
+</script>
