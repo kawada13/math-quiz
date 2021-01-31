2107170e4d79b9156f572b20176606e0b4bb69f7
kawada <vgnsl94@gmail.com>
2021-01-31 18:13:54 +0900

    計算式作成

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,12 +1,55 @@
 <template>
   <div>
-    <p>{{operator}}</p>
+    <div v-if="isQuizStart">
+      <h4>{{left}} {{operator}} {{right}}</h4>
+      <hr>
+      <button v-for="(answer, i) in answers" :key="i">{{answer}}</button>
+    </div>
+    <hr>
+    <div v-if="!isQuizStart">
+      <button @click="start">start</button>
+    </div>
     <button @click="$emit('clear')">Back</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['operator']
+  props: ['operator'],
+  data() {
+    return {
+      isQuizStart: false,
+      left: null,
+      right: null,
+      answers: []
+    }
+  },
+  methods: {
+    start() {
+      this.isQuizStart = true
+      this.left = parseInt(Math.random() * 13)
+      this.right = parseInt(Math.random() * 13)
+      for(let i = 0; i < 5; i++) {
+        const answer =
+        this.left * parseInt(Math.random() * 3) +
+        this.right * parseInt(Math.random() * 3) 
+
+        this.answers.push(answer)
+      }
+    }
+  },
 }
 </script>
+
+<style scoped>
+button {
+  padding: 10px;
+  height: 80px;
+  margin-right: 10px;
+  background-color: #10aeb2;
+  color: white;
+  font-size: 24px;
+  cursor: pointer;
+  vertical-align: top;
+}
+</style>
